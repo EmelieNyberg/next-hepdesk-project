832263c8b4f2b9596601c53febe8bedfd62da1b3
Emelie Nyberg <emelie.kedert@gmail.com>
2026-03-09 11:22:23 +0100

    fix: styling on ticket page

--- components/tickets/delete-ticket-form.tsx
+++ components/tickets/delete-ticket-form.tsx
@@ -13,7 +13,7 @@ export default function DeleteTicketForm({ id }: DeleteTicketFormProps) {
         <Form action={deleteTicket}>
             {/* Added this input to make sure we send correct id back. Its hidden so you want be able to see its value */}
             <input hidden readOnly name="id" value={id} />
-            <button type="submit" className="flex gap-4 items-center"><LuTrash2 /></button>
+            <button type="submit" className="cursor-pointer p-3 rounded-full hover:bg-(--color-component) transition"><LuTrash2 /></button>
 
         </Form>
     )
