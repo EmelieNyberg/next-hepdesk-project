10980ccb58e567448ed83d3cd33f58a52e185f7c
Emelie Nyberg <emelie.kedert@gmail.com>
2026-03-12 21:49:24 +0100

    feat: admin/user picture

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,13 +1,15 @@
 // components/navbar/navbar.tsx
 
 import Link from 'next/link'
+import Image from "next/image";
 import { RiTicketFill } from "react-icons/ri";
 
 export default function Navbar() {
     return (
         <header>
-            <nav className="flex gap-4 m-3 py-4 border-b border-white/10">
+            <nav className="flex gap-4 justify-between m-3 py-4 border-b border-white/10">
 
+                {/* Logo */}
                 <Link href="/" className="flex items-center gap-2 text-xl">
                     <RiTicketFill className="text-xl text-(--text-clr)" />
 
@@ -15,6 +17,16 @@ export default function Navbar() {
                         TicketFlow
                     </span>
                 </Link>
+
+                {/* Profile image */}
+                <Image
+                    src="/profile-pic.png"
+                    alt="User avatar"
+                    width={36}
+                    height={36}
+                    className="rounded-full"
+                />
+
             </nav>
         </header>
     )
